8bb9aab57a258ccd80a62339952f7dfe5b84f45c
myusername <o.yangol@g.nsu.ru>
2020-04-29 20:09:36 +0700

    initial

--- src/service/RestService.js
+++
@@ -1,8 +0,0 @@
-import Axios from "axios";
-
-export default class RestService {
-
-    static getRecipes() {
-        return Axios.get('http://localhost:10000/recipes');
-    }
-}
---
+++ vue-router/src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import home from '@/components/home'
+import blog from '@/components/blog'
+import services from '@/components/services'
+import contact from '@/components/contact'
+
+Vue.use(Router)
+export default new Router({
+  routes: [
+    {
+      path: '/', //компонент по умолчанию
+      name: 'home',
+      component: home
+    },
+    {
+      path: '/blog',
+      name: 'blog',
+      component: blog
+    },
+    {
+      path: '/services',
+      name: 'services',
+      component: services
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: contact
+    }
+  ]
+})
