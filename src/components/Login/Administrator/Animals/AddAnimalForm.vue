742c5c64d93ce23ed40490e5620b1f7d90c56234
myusername <o.yangol@g.nsu.ru>
2020-05-05 16:23:46 +0700

    wip

--- src/components/Login/Administrator/Animals/AddAnimalForm.vue
+++ src/components/Login/Administrator/Animals/AddAnimalForm.vue
@@ -1,32 +1,43 @@
 <template>
-  <div class="filters breadcrumb">
-    <button type="button" class="btn btn-primary">Показать список</button>
+  <div class="breadcrumb container ">
+    <h5>Добавление нового животного</h5>
+    <div class="container-item">
+      <div class="form-group">
+        <label class="col-form-label" for="inputDefault">Введите имя животного</label>
+        <input type="text" class="form-control" placeholder="Имя животного" id="inputDefault">
+      </div>
+    </div>
+    <button type="button" class="btn btn-primary">Добавить</button>
   </div>
 </template>
 
 <script>
-
+    import GenderChoice from "./GenderChoice";
     export default {
         name: 'addAnimalForm',
         data() {
-            return {
-            }
+            return {}
         },
-        methods: {
-        },
+        methods: {},
         components: {
+            GenderChoice
         }
     }
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  h1 {
-    text-align: center;
-  }
-  .filters {
-    height: 500px;
+  .breadcrumb {
     width: 94%;
-    margin-left: 3% ;
+    margin-left: 3%;
   }
 
+  .container {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .container-item {
+    display: flex;
+    flex-direction: row;
+  }
 </style>
