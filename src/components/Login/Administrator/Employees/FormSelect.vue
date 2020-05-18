7fa6a8641e424d947cff51429a91a1cf9be11947
myusername <o.yangol@g.nsu.ru>
2020-05-18 23:36:22 +0700

    переструктурирование формы для фильтрации списка сотрудников

--- src/components/Login/Administrator/Employees/FormSelect.vue
+++ src/components/Login/Administrator/Employees/FormSelect.vue
@@ -1,28 +1,106 @@
 <template>
   <div class="filters breadcrumb">
-    <RoleChoice class="container-item"></RoleChoice>
-    <GenderChoice class="container-item"></GenderChoice>
-    <SalaryChoice class="container-item"></SalaryChoice>
+
+    <!--выбор профессии служащего-->
+    <div class="container-item">
+      <label for="roleSelect"><h5>Выберите профессию</h5></label>
+      <select class="form-control" id="roleSelect" v-model="dto.role">
+        <option>Не выбрано</option>
+        <option>Администратор</option>
+        <option>Ветеринар</option>
+        <option>Дрессировщик</option>
+        <option>Уборщик</option>
+        <option>Строитель</option>
+      </select>
+    </div>
+
+    <!--выбор пола служащего-->
+    <div class="container-item">
+      <h5> Выберите пол служащего </h5>
+      <div class="btn-group btn-group-toggle" data-toggle="buttons">
+        <label class="btn btn-primary"
+               :class="{ active: male }"
+               @click="onMaleClick">
+          <input type="radio" name="options" id="option1" autocomplete="off" checked=""> М
+        </label>
+        <label class="btn btn-primary" :class="{ active: female }" @click="onFemaleClick">
+          <input type="radio" name="options" id="option2" autocomplete="off"> Ж
+        </label>
+      </div>
+    </div>
+
+    <h5> Выберите месячную зарплату служащего </h5>
+    <div class="form-group has-success container-item">
+      <label class="form-control-label">От</label>
+      <input type="text" placeholder="Зарплата служащего"
+             v-model="dto.lowSalary"
+             :class="{'is-valid': isValidLow, 'is-invalid': isInvalidLow}"
+             @change="checkIsNumberLow"
+             class="form-control"
+             id="inputValid">
+      <div class="valid-feedback">Success</div>
+      <div class="invalid-feedback">It's not a number</div>
+    </div>
+
+    <div class="form-group has-danger container-item">
+      <label class="form-control-label">До</label>
+      <input type="text" placeholder="Зарплата служащего"
+             v-model="dto.highSalary"
+             :class="{'is-valid': isValidHigh, 'is-invalid': isInvalidHigh}"
+             @change="checkIsNumberHigh"
+             class="form-control"
+             id="inputInvalid">
+      <div class="valid-feedback">Success</div>
+      <div class="invalid-feedback">It's not a number</div>
+    </div>
+
     <button type="button" class="btn btn-primary">Показать список</button>
   </div>
 </template>
 
 <script>
-    import RoleChoice from "./RoleChoice";
-    import GenderChoice from "./GenderChoice";
-    import SalaryChoice from "./SalaryChoice";
-
     export default {
-        name: 'formselect',
+        name: 'formSelect',
         data() {
-            return {}
+            return {
+                male: false,
+                female: false,
+
+                isValidLow: false,
+                isInvalidLow: false,
+
+                isValidHigh: false,
+                isInvalidHigh: false,
+
+                dto: {
+                    role: '',
+                    gender: '',
+                    lowSalary: '',
+                    highSalary: '',
+                }
+            }
         },
-        methods: {},
-        components: {
-           RoleChoice,
-            GenderChoice,
-            SalaryChoice
-        }
+        methods: {
+            onMaleClick: function () {
+                this.male = true;
+                this.female = false;
+                this.dto.gender = 'м';
+            },
+            onFemaleClick: function () {
+                this.male = false;
+                this.female = true;
+                this.dto.gender = 'ж';
+            },
+            checkIsNumberLow() {
+                this.isValidLow = !isNaN(this.dto.lowSalary);
+                this.isInvalidLow = isNaN(this.dto.lowSalary);
+            },
+            checkIsNumberHigh() {
+                this.isValidHigh = !isNaN(this.dto.highSalary);
+                this.isInvalidHigh = isNaN(this.dto.highSalary);
+            }
+        },
+        components: {}
     }
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -42,7 +120,7 @@
     margin-left: 5%;
   }
 
-  .btn{
+  .btn {
     width: 80%;
     margin-left: 5%;
     height: 40px;
